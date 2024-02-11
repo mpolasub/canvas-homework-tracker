55dfbbeb1442e66ccf2e7c6855675698fc539fd5
mpolasub <nondhawit@gmail.com>
2024-02-11 03:50:06 -0800

    stuck on finding toggler

--- main.py
+++ main.py
@@ -14,14 +14,24 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://canvas.uw.edu/courses/")
 
-input("Please log in.")
+input("Press 'enter' once you nave logged in.")
 
 all_courses = driver.find_elements(By.CLASS_NAME, value="course-list-table-row")
 term_courses = []
 
 for course in all_courses:
     if TERM in course.text:
-        term_courses.append(course)
+        term_courses.append(course.find_element(By.CSS_SELECTOR, value="a"))
 
-print(all_courses)
 print(term_courses)
+# term_courses[0].click()
+#
+for course in term_courses:
+    course.click()
+    time.sleep(2)
+    assignments_tab = driver.find_element(By.LINK_TEXT, value="Assignments")
+    assignment_toggle_button = driver.find_element(By.CSS_SELECTOR, value="[aria-controls='assignment_group_upcoming_assignments']")
+    print(assignment_toggle_button.get_attribute('aria-expanded'))
+    # if not assignment_toggle_button.get_attribute('aria-expanded'):
+    #     assignment_toggle_button.click()
+
