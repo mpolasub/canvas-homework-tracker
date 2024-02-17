c01e13ec119ac9ac26059aa08d83dd7e63abd939
mpolasub <nondhawit@gmail.com>
2024-02-17 02:40:36 -0800

    finished data collecting part, just need to clean up code and hook it up to sheety

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 import datetime
 
@@ -40,43 +41,64 @@ for course in all_courses:
     if TERM in course.text:
         term_courses.append(course.find_element(By.CSS_SELECTOR, value="a"))
 
-print(term_courses)
+# print(term_courses)
+
 
 # cycling through all courses
-for course in term_courses:
-    course_name = course.get_attribute("title")
-    all_assignments[course_name] = {}
-    course.click()
-    time.sleep(2)
-    assignments_tab = driver.find_element(By.LINK_TEXT, value="Assignments")
-    assignments_tab.click()
-    time.sleep(2)
-    # assignment_toggle_button = driver.find_element(By.CSS_SELECTOR, value="div div div div div ul li div div h2 button")
-    # time.sleep(5)
-    # if not assignment_toggle_button.get_attribute('aria-expanded'):
-    #     assignment_toggle_button.click()
+for i in range(len(term_courses)):
+    all_courses = driver.find_elements(By.CLASS_NAME, value="course-list-table-row")
+    term_courses = []
 
-    # TODO: create Course object by passing in course
-    # get course.assignments <- dict datatype
+    # get all relevant courses and put them in term_courses
+    for course in all_courses:
+        if TERM in course.text:
+            term_courses.append(course.find_element(By.CSS_SELECTOR, value="a"))
+
+    try:
+        course = term_courses[i]
+        print(course)
+        course_name = course.get_attribute("title")  # ALTERNATIVE: course_name = course.find_element(By.CLASS_NAME, value="name")
+        all_assignments[course_name] = {}
+        course.click()
+        time.sleep(2)
+        assignments_tab = driver.find_element(By.LINK_TEXT, value="Assignments")
+        assignments_tab.click()
+        time.sleep(3)
 
 
-    u_assignments_group = driver.find_element(By.CLASS_NAME, value="assignment-list")
-    assignments_info = u_assignments_group.find_elements(By.CLASS_NAME, value="ig-info")
-    for assignment in assignments_info:
-        assignment_name = assignment.text
-        all_assignments[course_name][assignment_name] = {}
-        due_date = driver.find_element(By.CSS_SELECTOR, value="div[class='ig-details__item assignment-date-due'] span").text
-        all_assignments[course_name][assignment_name]["due"] = due_date
-        all_assignments[course_name][assignment_name]["days_left"] = get_days_left(due_date)
+        u_assignments_group = driver.find_element(By.CLASS_NAME, value="assignment-list")
+        assignments_info = u_assignments_group.find_elements(By.CLASS_NAME, value="ig-info")
 
-    print(all_assignments)
+        for assignment in assignments_info:
+            assignment_text = assignment.text.split("\n")
+            assignment_name = assignment_text[0]
+            points = assignment_text[-2]
+            all_assignments[course_name][assignment_name] = {}
+            due_date = assignment.find_elements(By.CSS_SELECTOR, value=".ig-details__item span")[1].text
+            all_assignments[course_name][assignment_name]["due"] = due_date
+            all_assignments[course_name][assignment_name]["days_left"] = get_days_left(due_date)
+            all_assignments[course_name][assignment_name]["points"] = points
+
+
+        main_page = driver.find_element(By.ID, value="global_nav_courses_link")
+        main_page.click()
+        time.sleep(1)
+        courses_page = driver.find_element(By.LINK_TEXT, value="All Courses")
+        courses_page.click()
+        time.sleep(1)
+
+    except StaleElementReferenceException:
+        pass
+
+print(all_assignments)
 
 """"" JSON Format
 full_course = {
     "course_name1": {
         "assignment_name": {
             "due": "Feb 14 at 11:59PM",
-            "days_left": days_left
+            "days_left": days_left,
+            "points": points
         }
     },
     "course_name2": {
@@ -93,3 +115,10 @@ full_course = {
 """""
 
 # TODO: create Course class that breaks down data of each course
+
+
+
+"""
+except selenium.common.exceptions.StaleElementReferenceException:
+    pass
+"""
