e715c724a88ea2fa2a648dd049a726af8102b940
mpolasub <nondhawit@gmail.com>
2024-02-22 03:07:46 -0800

    started on csv section

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import StaleElementReferenceException
 import time
 import datetime
+import pandas as pd
 
 TERM = "Winter 2024"
 
 all_assignments = {}
+
 
 # keep chrome open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -31,21 +33,32 @@ def get_days_left(due_text):  # EX: due_text = "Mar 17 at 11:59pm"
     return days_left
 
 
+def get_assignment_details(hws):
+    for assignment in hws:
+        assignment_text = assignment.text.split("\n")
+        assignment_name = assignment_text[0]
+        points = assignment_text[-2]
+        if points.split()[0] == "Due":
+            points = "None"
+        all_assignments[course_name][assignment_name] = {}
+        due_date = assignment.find_elements(By.CSS_SELECTOR, value=".ig-details__item span")[1].text
+        all_assignments[course_name][assignment_name]["due"] = due_date
+        all_assignments[course_name][assignment_name]["days_left"] = get_days_left(due_date)
+        all_assignments[course_name][assignment_name]["points"] = points
+
+
 input("Press 'enter' once you have logged in.")
 
 all_courses = driver.find_elements(By.CLASS_NAME, value="course-list-table-row")
-term_courses = []
+num_course = 0
 
 # get all relevant courses and put them in term_courses
 for course in all_courses:
     if TERM in course.text:
-        term_courses.append(course.find_element(By.CSS_SELECTOR, value="a"))
-
-# print(term_courses)
-
+        num_course += 1
 
 # cycling through all courses
-for i in range(len(term_courses)):
+for i in range(num_course):
     all_courses = driver.find_elements(By.CLASS_NAME, value="course-list-table-row")
     term_courses = []
 
@@ -57,36 +70,28 @@ for i in range(len(term_courses)):
     try:
         course = term_courses[i]
         print(course)
-        course_name = course.get_attribute("title")  # ALTERNATIVE: course_name = course.find_element(By.CLASS_NAME, value="name")
+        course_name = course.get_attribute("title")
         all_assignments[course_name] = {}
         course.click()
         time.sleep(2)
+
         assignments_tab = driver.find_element(By.LINK_TEXT, value="Assignments")
         assignments_tab.click()
         time.sleep(3)
 
-
         u_assignments_group = driver.find_element(By.CLASS_NAME, value="assignment-list")
         assignments_info = u_assignments_group.find_elements(By.CLASS_NAME, value="ig-info")
 
-        for assignment in assignments_info:
-            assignment_text = assignment.text.split("\n")
-            assignment_name = assignment_text[0]
-            points = assignment_text[-2]
-            all_assignments[course_name][assignment_name] = {}
-            due_date = assignment.find_elements(By.CSS_SELECTOR, value=".ig-details__item span")[1].text
-            all_assignments[course_name][assignment_name]["due"] = due_date
-            all_assignments[course_name][assignment_name]["days_left"] = get_days_left(due_date)
-            all_assignments[course_name][assignment_name]["points"] = points
-
+        get_assignment_details(assignments_info)
 
         main_page = driver.find_element(By.ID, value="global_nav_courses_link")
         main_page.click()
         time.sleep(1)
+
         courses_page = driver.find_element(By.LINK_TEXT, value="All Courses")
         courses_page.click()
         time.sleep(1)
-
+    #
     except StaleElementReferenceException:
         pass
 
@@ -114,11 +119,7 @@ full_course = {
 }
 """""
 
-# TODO: create Course class that breaks down data of each course
 
-
-
-"""
-except selenium.common.exceptions.StaleElementReferenceException:
-    pass
-"""
+# CSV PART YK
+with open("assignments.csv") as hw_file:
+    data = pd.read_csv(hw_file)
