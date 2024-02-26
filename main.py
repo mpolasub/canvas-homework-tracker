14cab24732bcc7baf0d03e843bc760e0b972ba7c
mpolasub <nondhawit@gmail.com>
2024-02-26 03:20:35 -0800

    almost done with csv

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from selenium.common.exceptions import StaleElementReferenceException
 import time
 import datetime
 import pandas as pd
+import os
 
 TERM = "Winter 2024"
 
 all_assignments = {}
-
+assignment_list = []
 
 # keep chrome open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -37,18 +38,25 @@ def get_days_left(due_text):  # EX: due_text = "Mar 17 at 11:59pm"
     return days_left
 
 
-def get_assignment_details(hws):
+def get_assignment_details(hws, name):
     for assignment in hws:
         assignment_text = assignment.text.split("\n")
         assignment_name = assignment_text[0]
         points = assignment_text[-2]
         if points.split()[0] == "Due":
             points = "None"
+        assignment_link = assignment.find_element(By.LINK_TEXT, value=assignment_name)
+        # if "â€”" in assignment_name:
+        #     assignment_name.replace("â€”", "--")
         all_assignments[course_name][assignment_name] = {}
         due_date = assignment.find_elements(By.CSS_SELECTOR, value=".ig-details__item span")[1].text
         all_assignments[course_name][assignment_name]["due"] = due_date
-        all_assignments[course_name][assignment_name]["days_left"] = get_days_left(due_date)
+        days_left = get_days_left(due_date)
+        all_assignments[course_name][assignment_name]["days_left"] = days_left
         all_assignments[course_name][assignment_name]["points"] = points
+
+        assignment_tuple = (assignment_name, due_date, days_left, points, name, assignment_link)
+        assignment_list.append(assignment_tuple)
 
 
 input("Press 'enter' once you have logged in.")
@@ -86,7 +94,7 @@ for i in range(num_course):
         u_assignments_group = driver.find_element(By.CLASS_NAME, value="assignment-list")
         assignments_info = u_assignments_group.find_elements(By.CLASS_NAME, value="ig-info")
 
-        get_assignment_details(assignments_info)
+        get_assignment_details(assignments_info, course_name)
 
         main_page = driver.find_element(By.ID, value="global_nav_courses_link")
         main_page.click()
@@ -123,8 +131,7 @@ full_course = {
 }
 """""
 
+# populating csv
 
-# CSV PART YK
-file = open("assignments.csv", mode="w")
-file.close()
-data = pd.read_csv("assignments.csv")
+hw_df = pd.DataFrame(assignment_list, columns=['Assignments', 'Due Date', 'Days Left', 'Points', 'Course Name', 'Link'])
+hw_df.to_csv('assignments.csv', encoding='utf-8')
